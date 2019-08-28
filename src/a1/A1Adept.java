c035ddc578a1cc1c222d633c52a851c9a13650fe
Eric <esashton@ad.unc.edu>
2019-08-28 17:51:41 -0400

    Novice and Adept first attempt

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -10,5 +10,88 @@ public class A1Adept {
 
 		// Your code follows here.
 		
+		int numOfItems = scan.nextInt();
+		
+		String[] items = new String[numOfItems];
+		double[] prices = new double[numOfItems];
+		
+		for (int i = 0; i < numOfItems; i++) {
+			items[i] = scan.next();
+			prices[i] = scan.nextDouble();
+		}
+		
+		int numOfCustomers = scan.nextInt();
+		
+		String[] FirstNames = new String[numOfCustomers];
+		String[] LastNames = new String[numOfCustomers];
+		// int[] totalItems = new int[numOfCustomers];
+		double[] totals = new double[numOfCustomers];
+		
+		for (int j = 0; j < numOfCustomers; j++) {
+			FirstNames[j] = scan.next();
+			LastNames[j] = scan.next();
+			int totalItems = scan.nextInt();
+			int[] quantity = new int[totalItems];
+			String[] customerItems = new String[totalItems];
+			
+			for (int k = 0; k < totalItems; k++) {
+				quantity[k] = scan.nextInt();
+				customerItems[k] = scan.next();
+			}
+						
+			for (int l = 0; l < items.length; l++) {
+				for (int m = 0; m < totalItems; m++) {
+					if (customerItems[m].equals(items[l])) {
+						totals[j] = totals[j] + (quantity[m] * prices[l]);
+					}
+				}
+			}
+			
+			
+		}
+		
+		scan.close();
+		
+		// System.out.println(totals[1]);
+		
+		// double biggest = totals[0];
+		// double smallest = totals[0];
+		double average = avg(totals);
+		
+		
+		
+		System.out.println("Biggest: " + FirstNames[biggest(totals)] + " " + LastNames[biggest(totals)] + " " + String.format("%.2f", totals[biggest(totals)]));
+		System.out.println("Smallest: " + FirstNames[smallest(totals)] + " " + LastNames[smallest(totals)] + " " + String.format("%.2f", totals[smallest(totals)]));
+		System.out.println("Average: " + String.format("%.2f", average));
+		
+	}
+	
+	public static double avg(double[] a) {
+		double sum = 0;
+		for (int i = 0; i < a.length; i++) {
+			sum = sum + a[i];
+		}
+		
+		return sum / a.length;
+	}
+	
+	public static int biggest(double[] t) {
+		int biggest = 0;
+		for (int i = 0; i < t.length - 1; i++) {
+			if (t[i] < t[i + 1]) {
+				biggest = i + 1;
+			}
+		}
+		return biggest;
+	}
+	
+	public static int smallest(double[] t) {
+		int smallest = 0;
+		for (int i = 0; i < t.length - 1; i++) {
+			if (t[i] > t[i + 1]) {
+				smallest = i + 1;
+			}
+		}
+		return smallest;
 	}
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,35 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int numOfCustomers = scan.nextInt();
+		
+		String[] FirstNames = new String[numOfCustomers];
+		String[] LastNames = new String[numOfCustomers];
+		double[] totals = new double[numOfCustomers];
+		
+		
+		for (int i = 0; i < numOfCustomers; i++) {
+			FirstNames[i] = scan.next();
+			LastNames[i] = scan.next();
+			int numOfItems = scan.nextInt();
+			totals[i] = 0;
+			
+			for (int j = 0; j < numOfItems; j++) {
+				int quantity = scan.nextInt();
+				String name = scan.next();
+				double price = scan.nextDouble();
+				
+				totals[i] = totals[i] + (price * quantity);
+			}
+
+		}
+		
+		for (int i = 0; i < numOfCustomers; i++) {
+			System.out.println(FirstNames[i].charAt(0) + ". " + LastNames[i] + ": " + String.format("%.2f", totals[i]));
+		}
+			
+		scan.close();
+		
 	}
+	
 }
